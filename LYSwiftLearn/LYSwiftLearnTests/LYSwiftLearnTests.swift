1649f2f15bec1002ba8e864ee5f92b60fd67fae2
Liuyu <729789569@qq.com>
2020-05-14 21:00:53 +0800

    11

--- LYSwiftLearn/LYSwiftLearnTests/LYSwiftLearnTests.swift
+++ LYSwiftLearn/LYSwiftLearnTests/LYSwiftLearnTests.swift
@@ -20,8 +20,7 @@ class LYSwiftLearnTests: XCTestCase {
     }
 
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        print("=====")
     }
 
     func testPerformanceExample() {
